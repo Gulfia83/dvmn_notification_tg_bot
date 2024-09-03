9d951462c7b776bcabc72faef54090ca8052a57f
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-09-03 12:38:14 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ from dotenv import load_dotenv
 import logging
 
 
-logger = logging.getLogger('check_results')
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def send_message(bot, chat_id, results):
@@ -30,8 +39,10 @@ def main():
 
     bot = telegram.Bot(tg_bot_token)
 
+    logger = logging.getLogger('check_results')
     logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s")
     logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(bot, tg_chat_id))
     logger.info('Бот запущен')
 
     while True:
